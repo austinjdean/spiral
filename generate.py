e5df8dfaf106c1a13cb935340088782d9f2e358e
Austin Dean <austin.j.dean@lmco.com>
2016-09-09 11:40:17 -0400

    start tracking in github

---
+++ generate.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python
+
+from turtle import *
+import random
+
+frameHeight_g	= 750
+frameWidth_g	=  ((1 + sqrt(5)) / 2) * frameHeight_g # golden ratio
+vDivs_g			= 5
+hDivs_g			= 8
+divHeight_g		= frameHeight_g / vDivs_g
+divWidth_g		= frameWidth_g / hDivs_g
+canvas_g		= ""
+leftEdge_g		= 0
+topEdge_g		= 0
+rightEdge_g		= 0
+bottomEdge_g	= 0
+
+def drawCanvas():
+	global canvas_g, leftEdge_g, topEdge_g, rightEdge_g, bottomEdge_g
+	speed(0)
+	hideturtle()
+
+	penup()
+	goto(-frameWidth_g / 2, -frameHeight_g / 2)
+	pendown()
+
+	begin_poly()
+	setheading(0)
+	forward(frameWidth_g)
+	left(90)
+	forward(frameHeight_g)
+	left(90)
+	forward(frameWidth_g)
+	left(90)
+	end_poly()
+	forward(frameHeight_g)
+	left(90)
+	canvas_g		= get_poly()
+	leftEdge_g		=  canvas_g[0][0] # all edge info can be derived from first (bottom left) point because canvas is centered
+	topEdge_g		= -canvas_g[0][1]
+	rightEdge_g		= -canvas_g[0][0]
+	bottomEdge_g	=  canvas_g[0][1]
+
+def spiral(polygon, startingCorner, direction, deviance):
+	pass
+	# polygon is which shape to spiral
+	# startingCorner is which corner to start in
+	# direction is clockwise or counterclockwise
+	# deviance is how many pixels away from the current edge the end of the line will be
+	# 	deviance could also be done as:
+	# 	- a percentage of the opposing side
+	# 	- a percentage of the angle
+	# 	- a raw angle (e.g. 5 degrees)
+	# 	should we have options for all of these?
+
+	# function outline:
+	# pen up
+	# jump to starting corner
+	# 
+	# definitely gonna be using towards(x, y)
+	# so how to get x, y?
+
+def populateInteriorPoints():
+	heightMarkers = []
+	widthMarkers = []
+
+	penup()
+	goto(canvas_g[0]) # first element is bottom left corner because of how it's drawn
+	setheading(90)
+
+	# vertical first
+	for i in range(0, vDivs_g - 1): # minus one to discard first and last
+		forward(divHeight_g)
+		# dot()
+		heightMarkers.append(ycor())
+
+	# horizontal next
+	goto(canvas_g[0]) # first element is bottom left corner because of how it's drawn
+	setheading(0)
+
+	for i in range(0, hDivs_g - 1): # minus one to discard first and last
+		forward(divWidth_g)
+		# dot()
+		widthMarkers.append(xcor())
+
+	# thanks: http://stackoverflow.com/a/19334433/2929868
+	coordinates = [(x,y) for x in widthMarkers for y in heightMarkers]
+	return coordinates
+
+def populateExteriorPoints():
+	coordinates = []
+	goto(canvas_g[0]) # first element is bottom left corner because of how it's drawn
+	setheading(0)
+	# bottom edge
+	for i in range(0, hDivs_g):
+		# dot()
+		coordinates.append(position())
+		forward(divWidth_g)
+	left(90)
+	# right edge
+	for i in range(0, vDivs_g):
+		# dot()
+		coordinates.append(position())
+		forward(divHeight_g)
+	left(90)
+	# top edge
+	for i in range(0, hDivs_g):
+		# dot()
+		coordinates.append(position())
+		forward(divWidth_g)
+	left(90)
+	# left edge
+	for i in range(0, vDivs_g):
+		# dot()
+		coordinates.append(position())
+		forward(divHeight_g)
+
+	return coordinates
+
+def skewPoints(points):
+	skewedPoints = []
+	lowerSkewBound = -30 # make these changeable later
+	upperSkewBound = 30
+	for point in points:
+		skewedPoint = ()
+		for component in point:
+			skewFactor = random.uniform(lowerSkewBound, upperSkewBound)
+			component = component + skewFactor
+			skewedPoint += (component,)
+		skewedPoints.append(skewedPoint)
+	return skewedPoints
+
+def main():
+	drawCanvas()
+
+	# For all positive integers NUM and DEN <= 10, where NUM > DEN, the combination 
+	# 8/5 (1.6) yielded the closest decimal approximation to the golden ratio (1.61803398875).
+	# Therefore, a canvas of golden ratio proportions can be divided using 8 segments
+	# on the long side, and 5 segments on the short side. The cells made as a result
+	# of the segmented sides are the closest to squares that such cells could get
+	# without altering the canvas proportions.
+
+	interiorPoints = populateInteriorPoints()
+	skewedInteriorPoints = skewPoints(interiorPoints)
+
+	# for point in skewedInteriorPoints:
+		# goto(point)
+		# dot()
+
+	exteriorPoints = populateExteriorPoints()
+
+	allPoints = skewedInteriorPoints + exteriorPoints
+
+	# for point in allPoints:
+	# 	goto(point)
+	# 	dot()
+		# print point
+
+	allPoints.sort()
+
+	speed(2)
+
+	for point in allPoints:
+		goto(point)
+		dot()
+		print point
+
+	exitonclick()
+
+if __name__ == '__main__':
+	main()
+
+# setheading(towards(0, 0))
+# goto(0, 0)
